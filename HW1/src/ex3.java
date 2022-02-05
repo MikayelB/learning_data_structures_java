6643872e14809d3c018422e97153b7c47161fa1d
Mikayel <mikael.bejanyan@gmail.com>
2022-02-05 17:30:10 +0400

    almost done, needs to fix the returning of the number of steps

--- HW1/src/ex3.java
+++ HW1/src/ex3.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class ex3 {
     public static void main(String[] args) {
 
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the length of the matrix's side (must be odd): ");
@@ -13,10 +14,10 @@ public class ex3 {
         fillMatrixData(input, matrix, matrixLength);
         printMatrix(matrix, matrixLength);
 
-        int middle = (int)Math.ceil((matrixLength / 2));
-        System.out.println(middle);
+        int middle = (int) Math.ceil((matrixLength / 2));
+//        System.out.println(middle);
         int matrixMid = matrix[middle][middle];
-        System.out.println(matrixMid);
+//        System.out.println(matrixMid);
 
         movingToCenter(matrix, matrixLength, middle);
         printMatrix(matrix, matrixLength);
@@ -24,41 +25,62 @@ public class ex3 {
 
     }
 
-    public static int[][] movingToCenter(int[][] matrix, int size, int mid){
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                if(matrix[i][j] ==  0){
-                    if(j < mid){
-                        int temp = matrix[i][j+1];
-                        matrix[i][j+1] = matrix[i][j];
+    public static int movingToCenter(int[][] matrix, int size, int mid) {
+        int stepCount = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (matrix[i][j] == 0) {
+                    if (j < mid) {
+                        int temp = matrix[i][j + 1];
+                        matrix[i][j + 1] = matrix[i][j];
                         matrix[i][j] = temp;
+                        stepCount++;
+
                     }
-                    if(j > mid){
-                        int temp = matrix[i][j-1];
-                        matrix[i][j-1] = matrix[i][j];
+                    if (j > mid) {
+                        int temp = matrix[i][j - 1];
+                        matrix[i][j - 1] = matrix[i][j];
                         matrix[i][j] = temp;
-                        j-= 2;
+                        j -= 2;              // since the loop is moving forward we need this to get the j back on track
+                        stepCount++;
+
                     }
+                }
+            }
+            if (matrix[i][mid] == 0) {
+                if (i < mid) {
+                    int temp = matrix[i + 1][mid];
+                    matrix[i + 1][mid] = matrix[i][mid];
+                    matrix[i][mid] = temp;
+                    stepCount++;
+
+                }
+                if (i > mid) {
+                    int temp = matrix[i - 1][mid];
+                    matrix[i - 1][mid] = matrix[i][mid];
+                    matrix[i][mid] = temp;
+                    stepCount++;
+
                 }
             }
         }
 
-        return matrix;
+        return stepCount;
 
     }
 
-    public static void fillMatrixData(Scanner input, int[][] matrix, int size){
+    public static void fillMatrixData(Scanner input, int[][] matrix, int size) {
         System.out.println("Enter the matrix's data: ");
 
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 matrix[i][j] = input.nextInt();
             }
         }
     }
 
-    public static void printMatrix(int[][] matrix, int size){
-        for(int i = 0; i < size; i++) {
+    public static void printMatrix(int[][] matrix, int size) {
+        for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 System.out.print(matrix[i][j] + "\t");
             }
