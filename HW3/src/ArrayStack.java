4ee0e8db270455a384862d19c5e8f3875c742cb8
Mikayel <mikael.bejanyan@gmail.com>
2022-03-08 23:19:44 +0400

    ex5 completed: ArrayStack implemented as well as ex5 main test functions

--- HW3/src/ArrayStack.java
+++ HW3/src/ArrayStack.java
@@ -1,2 +1,86 @@
-public class ArrayStack {
+import sources.Stack;
+
+public class ArrayStack<E> implements Stack<E> {
+    public static final int CAPACITY=1000;
+    private E[] arr;
+    private int top = -1;
+    private int size = 0;
+
+    //----constructors----
+    public ArrayStack() { this(CAPACITY); }
+
+    public ArrayStack(int capacity) {
+        arr = (E[]) new Object[capacity];
+    }
+    //--------------------
+
+    public void resize(){
+        if(size <(arr.length / 4)){
+            E[] temp = (E[]) new Object[arr.length / 2];
+
+            for(int i = 0; i < temp.length; i++){
+                temp[i] = arr[i];
+            }
+
+            arr = temp;
+
+        } else if (size == arr.length){
+            E[] temp = (E[]) new Object[arr.length * 2];
+
+            for(int i = 0; i < arr.length; i++){
+                temp[i] = arr[i];
+            }
+
+            arr = temp;
+        } else {
+            return;
+        }
+    }
+
+    @Override
+    public int size() {
+//        System.out.println("arr.length = " + arr.length);  //test
+        return (size);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    @Override
+    public void push(E e) {
+        resize();
+        arr[++top] = e;
+        size++;
+
+    }
+
+    @Override
+    public E top() {
+        if (isEmpty()) return null;
+        return arr[top];
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) return null;
+        E answer = arr[top];
+        arr[top] = null;
+        top--;
+        size--;
+        resize();
+        return answer;
+    }
+
+    //---Helper functions
+    public String toString() {
+        StringBuilder sb = new StringBuilder("(");
+        for (int j = top; j >= 0; j--) {
+            sb.append(arr[j]);
+            if (j > 0) sb.append(", ");
+        }
+        sb.append(")");
+        return sb.toString();
+    }
 }
--- HW3/src/ex5.java
+++ HW3/src/ex5.java
@@ -1,2 +1,51 @@
 public class ex5 {
+    public static void main(String[] args) {
+        ArrayStack<Integer> stack = new ArrayStack<Integer>(10);
+
+        System.out.println("-----size-----");
+        System.out.println(stack.size());
+
+        System.out.println("-----isEmpty-----");
+        System.out.println(stack.isEmpty());
+
+        System.out.println("-----push-----");
+        stack.push(2);
+        stack.push(4);
+        stack.push(3);
+        System.out.println("push(2), push(4), push(3)");
+        System.out.println("stack.size(): " + stack.size());
+        System.out.println("The stack at the moment: " + stack);
+
+        System.out.println("-----top-----");
+        System.out.println(stack.top());
+
+
+        stack.push(6);
+        stack.push(123);
+        stack.push(6123);
+        System.out.println("push(6), push(123), push(6123)");
+        System.out.println("The stack at the moment: " + stack);
+
+        System.out.println("-----top-----");
+        System.out.println(stack.top());
+
+        System.out.println("stack.size(): " + stack.size());
+
+        System.out.println("-----isEmpty-----");
+        System.out.println(stack.isEmpty());
+
+        System.out.println("-----pop-----");
+        stack.pop();
+        System.out.println("The stack at the moment: " + stack);
+
+        System.out.println("-----top-----");
+        System.out.println(stack.top());
+
+        System.out.println("-----pop-----");
+        stack.pop();
+        System.out.println("The stack at the moment: " + stack);
+
+        System.out.println("-----top-----");
+        System.out.println(stack.top());
+    }
 }
