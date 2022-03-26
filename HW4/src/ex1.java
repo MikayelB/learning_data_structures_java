82e9221bcd2b999791b7c725de255e4b235f070c
Mikayel <mikael.bejanyan@gmail.com>
2022-03-26 17:43:56 +0400

    Minor improvements, added PositionalList function base for ex1

--- HW4/src/ex1.java
+++ HW4/src/ex1.java
@@ -1,27 +1,31 @@
 import sources.ArrayList;
+import sources.LinkedPositionalList;
 
 import java.lang.reflect.Array;
 
 public class ex1 {
 
-    public static void addMissingLettersArrayList(ArrayList<Character> list){
+    public static void addMissingLettersArrayList(ArrayList<Character> arrList){  // O(n) more details in main
         int ascii = 122; // z
 
         for(int i = 0; i <= 25; i++){//z x
 
-            if((int)list.get(i) == ascii){
-                if(i == list.size() -1){
-                    list.add(list.size(), ' ');
+            if((int)arrList.get(i) == ascii){
+                if(i == arrList.size() -1){
+                    arrList.add(arrList.size(), ' ');
                 }
                 ascii--;
             }else {
-                list.add(i, (char) ascii);
+                arrList.add(i, (char) ascii);
                 ascii--;
             }
         }
-        list.remove(list.size() - 1);
+        arrList.remove(arrList.size() - 1);
     }
 
+    public static void addMissingLettersLinkedPositionalList(LinkedPositionalList<Character> posList){
+
+    }
     public static void main(String[] args) {
         ArrayList<Character> l = new ArrayList<>();
         l.add(0, 'm');
@@ -30,9 +34,10 @@ public class ex1 {
         l.add(3, 'b');
         l.add(4, 'a');
 
-        System.out.println(l);
+        System.out.println("ArrayList start: " + l);
+        // O(n), more specifically O(26) since it has to loop through the arraylist and add the missing letters which are up to 26 letters
         addMissingLettersArrayList(l);
-        System.out.println(l);
+        System.out.println("ArrayList end: " + l);
 
     }
 
