6d3f407f7da2d1c6731d4dfb7a84229810ce2d5d
Mikayel <mikael.bejanyan@gmail.com>
2022-03-28 12:22:15 +0400

    Added SLLList

---
+++ HW4/src/SLLList.java
@@ -0,0 +1,40 @@
+import sources.List;
+
+import java.util.Iterator;
+
+public class SLLList implements List {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Object get(int i) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public Object set(int i, Object o) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public void add(int i, Object o) throws IndexOutOfBoundsException {
+
+    }
+
+    @Override
+    public Object remove(int i) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
