8bca9229ee3748266832ca0ba1cbfc3aa071c689
Mikayel <mikael.bejanyan@gmail.com>
2022-03-29 20:44:45 +0400

    ex5 a) solved

--- HW4/src/ex5.java
+++ HW4/src/ex5.java
@@ -2,6 +2,7 @@ import sources.LinkedPositionalList;
 import sources.Position;
 
 import java.util.Calendar;
+import java.util.NoSuchElementException;
 
 public class ex5 {
     public static void main(String[] args) {
@@ -11,48 +12,73 @@ public class ex5 {
 
         list.addLast('c');
         list.addLast('d');
+        list.addLast('b');
         list.addLast('z');
         list.addLast('h');
         list.addLast('e');
         list.addLast('a');
 
         System.out.println(list);
-//        selectionSort(list);
+        selectionSort(list);
         System.out.println(list);
 
     }
 
     public static void selectionSort(LinkedPositionalList<Character> PL){
+//  garbage
+//
+//
+//        while(outerCurr != PL.last()) {
+//
+//            while (innerCurr != null) {
+//                if (innerCurr.getElement() < min.getElement()) {
+//                    min = innerCurr;
+//                }
+//                innerCurr = PL.after(innerCurr);
+//            }
+//            innerCurr = outerCurr;
+//
+////            if (min == first) {
+////                PL.after(outerCurr);
+////                return;
+////            }
+//            Position<Character> tempMin = min;
+//            char tempMinEl = tempMin.getElement();
+//
+////            PL.remove(min);
+//            PL.addBefore(outerCurr, PL.remove(min));
+//
+//            System.out.println("====================");
+//            min = PL.after(outerCurr);
+////            outerCurr = PL.after(outerCurr);
+//        }
 
         Position<Character> outerCurr = PL.first();
-        Position<Character> first = PL.first();
-            Position<Character> innerCurr = PL.first();
-            Position<Character> min = PL.first();
+        Position<Character> innerCurr = PL.first();
+        Position<Character> min = PL.first();
 
-
-        while(outerCurr != PL.last()) {
-
+        while(outerCurr != null) {
             while (innerCurr != null) {
                 if (innerCurr.getElement() < min.getElement()) {
                     min = innerCurr;
                 }
                 innerCurr = PL.after(innerCurr);
             }
+
+            if(min == outerCurr){
+                outerCurr = PL.after(outerCurr);
+                min = outerCurr;
+                innerCurr = outerCurr;
+                continue;
+            }
+
+            PL.addBefore(outerCurr, min.getElement());
+            PL.addAfter(PL.before(min), outerCurr.getElement());
+            PL.remove(min);
+            outerCurr = PL.after(outerCurr);
+            PL.remove(PL.before(outerCurr));
+            min = outerCurr;
             innerCurr = outerCurr;
-
-//            if (min == first) {
-//                PL.after(outerCurr);
-//                return;
-//            }
-            Position<Character> tempMin = min;
-            char tempMinEl = tempMin.getElement();
-
-//            PL.remove(min);
-            PL.addBefore(outerCurr, PL.remove(min));
-
-            System.out.println("====================");
-            min = PL.after(outerCurr);
-//            outerCurr = PL.after(outerCurr);
         }
 
     }
