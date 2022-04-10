e3c1b95f8931784beb3e3cb2bc3bd59d1c82d371
Mikayel <mikael.bejanyan@gmail.com>
2022-04-10 14:02:50 +0400

    Added first part of ex4

--- HW5/src/LinkedGeneralTree.java
+++ HW5/src/LinkedGeneralTree.java
@@ -12,18 +12,32 @@ public class LinkedGeneralTree<E> extends AbstractTree<E> {
         private LinkedBinaryTree.Node<E> parent;     // a reference to the parent node (if any)
         private ArrayList<Node<E>> children;            // a reference to the children arrayList
 
+
+        // constructor
         public Node(E e, LinkedBinaryTree.Node<E> above, ArrayList<Node<E>> children) {
             element = e;
             parent = above;
             children = new ArrayList<Node<E>>();
         }
 
+
         // accessor methods
         public E getElement() { return element; }
         public LinkedBinaryTree.Node<E> getParent() { return parent; }
         public ArrayList<Node<E>> getChildren() { return children; }
 
-        // update methods
+        // homework methods
+        // 1)
+        // a)
+        public Node<E> getChild(int i){ return children.get(i); }
+        // b)
+        public E removeChild(int i) {return children.remove(i).getElement();}
+        // c)
+        public Node<E> setChild(int i, Node<E> e){ return children.set(i, e);}
+        // d)
+        public void addChild(int i, Node<E> e){ children.add(i, e);}
+
+        // default update methods
         public void setElement(E e) { element = e; }
         public void setParent(LinkedBinaryTree.Node<E> parentNode) { parent = parentNode; }
     } //----------- end of nested Node class -----------
