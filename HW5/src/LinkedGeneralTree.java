01396f49cd0f26424f455bdf1e585b84d587a93e
Mikayel <mikael.bejanyan@gmail.com>
2022-04-10 17:40:24 +0400

    Ex4 to f) done

--- HW5/src/LinkedGeneralTree.java
+++ HW5/src/LinkedGeneralTree.java
@@ -2,19 +2,21 @@ import sources.AbstractTree;
 import sources.ArrayList;
 import sources.Position;
 
+import java.util.List;
+
 public class LinkedGeneralTree<E> extends AbstractTree<E> {
 
 
     //---------------- nested Node class ----------------
-    /** Nested static class for a binary tree node. */
+    /** Nested static class for a general tree node. */
     protected static class Node<E> implements Position<E> {
         private E element;          // an element stored at this node
-        private LinkedBinaryTree.Node<E> parent;     // a reference to the parent node (if any)
+        private Node<E> parent;     // a reference to the parent node (if any)
         private ArrayList<Node<E>> children;            // a reference to the children arrayList
 
 
         // constructor
-        public Node(E e, LinkedBinaryTree.Node<E> above, ArrayList<Node<E>> children) {
+        public Node(E e, Node<E> above, ArrayList<Node<E>> children) {
             element = e;
             parent = above;
             children = new ArrayList<Node<E>>();
@@ -23,7 +25,7 @@ public class LinkedGeneralTree<E> extends AbstractTree<E> {
 
         // accessor methods
         public E getElement() { return element; }
-        public LinkedBinaryTree.Node<E> getParent() { return parent; }
+        public Node<E> getParent() { return parent; }
         public ArrayList<Node<E>> getChildren() { return children; }
 
         // homework methods
@@ -39,25 +41,97 @@ public class LinkedGeneralTree<E> extends AbstractTree<E> {
 
         // default update methods
         public void setElement(E e) { element = e; }
-        public void setParent(LinkedBinaryTree.Node<E> parentNode) { parent = parentNode; }
+        public void setParent(Node<E> parentNode) { parent = parentNode; }
     } //----------- end of nested Node class -----------
 
 
 
+    // 2)
+    // a)
+    /** Factory function to create a new node storing element e. */
+    protected Node<E> createNode(E e, Node<E> parent,
+                                                  ArrayList<Node<E>> children) {
+        return new Node<E>(e, parent, children);
+    }
+
+    // b)
+    protected Node<E> validate(Position<E> p) throws IllegalArgumentException {
+        if (!(p instanceof Node))
+            throw new IllegalArgumentException("Not valid position type");
+        Node<E> node = (Node<E>) p;       // safe cast
+        if (node.getParent() == node)     // our convention for defunct node
+            throw new IllegalArgumentException("p is no longer in the tree");
+        return node;
+    }
+
+    // LinkedGeneralTree instance variables
+    protected Node<E> root = null;     // root of the tree
+    private int size = 0;              // number of nodes in the tree
+
+    // constructor
+    public LinkedGeneralTree() { }      // constructs an empty general tree
 
 
+    //------------ override required ones------------
     @Override
-    public Position root() {
-        return null;
+    public Position<E> root() {
+        return root;
     }
 
     @Override
-    public Position parent(Position p) throws IllegalArgumentException {
-        return null;
+    public Position<E> parent(Position<E> p) throws IllegalArgumentException {
+        Node<E> el = validate(p);
+        return el.getParent();
     }
 
     @Override
-    public Iterable<Position> children(Position p) throws IllegalArgumentException {
-        return null;
+    public Iterable<Position<E>> children(Position<E> p) throws IllegalArgumentException {
+
+        Node<E> node = validate(p);
+        int size = node.children.size();
+
+        List<Position<E>> snapshot = (List<Position<E>>) new ArrayList<Position<E>>();
+        for(int i = 0; i < size; i++){
+            snapshot.add(i, node.children.get(i));
+        }
+        return snapshot;
     }
+
+    //------------ override required ones end------------
+
+    // c)
+    public Position<E> addRoot(E e) throws IllegalStateException {
+        if (!isEmpty()) throw new IllegalStateException("Tree is not empty");
+        root = createNode(e, null, null);
+        size = 1;
+        return root;
+    }
+
+    // d)
+    public Position<E> ithChild(Position<E> p, int i){
+        if (!isEmpty()) throw new IllegalStateException("Tree is not empty");
+
+        Node<E> node = validate(p);
+        return node.getChild(i);
+    }
+
+    // e)
+    public Position<E> addIth(Position<E> p, E e, int i){
+        Node<E> node = validate(p);
+        Node<E> child = createNode(e, node, null);
+
+        node.addChild(i, child);
+        return node.getChild(i);
+    }
+
+    // f)
+    public Position<E> addFirst(Position<E> p, E e){
+        Node<E> node = validate(p);
+        Node<E> newFirstChild = createNode(e, node, null);
+
+        node.addChild(0, newFirstChild);
+        return node.getChild(0);
+    }
+
+
 }
