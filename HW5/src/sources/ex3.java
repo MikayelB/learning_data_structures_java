b14fec77a6dace9aff0af11dc4e779ca97f18815
Mikayel <mikael.bejanyan@gmail.com>
2022-04-10 23:52:43 +0400

    Added ex3 class but not working yet

---
+++ HW5/src/sources/ex3.java
@@ -0,0 +1,14 @@
+package sources;
+
+public class ex3 {
+
+
+
+//    public static Array<E> arrRepresentationOfATree(BinaryTree<E> tree){
+//
+//
+//    }
+    public static void main(String[] args) {
+
+    }
+}
