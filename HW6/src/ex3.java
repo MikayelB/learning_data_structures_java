37330012c623b12453f77c78c7c23e17aae7ae66
Mikayel <mikael.bejanyan@gmail.com>
2022-04-24 11:50:16 +0400

    Added ex3 incomplete tests

---
+++ HW6/src/ex3.java
@@ -0,0 +1,46 @@
+import sources.Entry;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ex3 {
+
+    // right child 2f(q) + 2
+    // left child 2f(q) + 1
+
+    public static <K,V> boolean isMaxHeap(Entry<K,V>[] arr, Comparator<K> comparator){
+
+        int lastIndex = arr.length - 1;
+        Entry<K,V> last = arr[lastIndex];
+        Entry<K,V> lastParent = arr[lastIndex - 1 / 2];
+
+        if(comparator.compare(last.getKey(), lastParent.getKey()) == 1){
+            if(isMaxHeap(Arrays.copyOfRange(arr, 0, lastIndex), comparator)){
+                return true;
+            };
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+
+
+        Entry[] arr  = new Entry[];
+        MaxPriorityQueue q = new MaxPriorityQueue();
+
+        q.insert('1', 1);
+        q.insert('2', 2);
+        q.insert('3', 3);
+        q.insert('4', 4);
+        q.insert('5', 5);
+        q.insert('6', 44);
+
+        while(!q.isEmpty()){
+            System.out.println(arr.push(q.removeMax().getValue()));
+        }
+
+    }
+}
